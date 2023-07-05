80e91c9b68d39dbf9f1bce4c50e78cf9c9e55666
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2023-07-05 10:34:52 -0300

    feat: export from PDF file

--- extrato/views.py
+++ extrato/views.py
@@ -37,19 +37,41 @@ def novo_valor(request):
         else:
             conta.valor -= int(valor)
 
+        #TODO: gerar uma mensagem de saída de acordo com o tipo ( se for saída ou entrada)
+
         conta.save()
 
-        
-
-        messages.add_message(request, constants.SUCCESS, 'Categoria cadastrada com sucesso')
+        messages.add_message(request, constants.SUCCESS, 'Cadastro realizado com sucesso!')
         return redirect('/extrato/novo_valor')
     
 def view_extrato(request):
     contas = Conta.objects.all()
     categorias = Categoria.objects.all()
-
-        
-    valores = Valores.objects.filter(data__month=datetime.now().month)
+    conta_get = request.GET.get('conta')
+    categoria_get = request.GET.get('categoria')
  
+    valores = Valores.objects.filter(data__month=datetime.now().month)
+
+    if conta_get:
+        valores = valores.filter(conta__id=conta_get)
+
+    if categoria_get:
+        valores = valores.filter(categoria__id=categoria_get)
 
     return render(request, 'view_extrato.html', {'valores': valores, 'contas': contas, 'categorias': categorias})
+
+def exportar_pdf(request):
+    valores = Valores.objects.filter(data__month=datetime.now().month)
+    contas = Conta.objects.all()
+    categorias = Categoria.objects.all()
+    
+    path_template = os.path.join(settings.BASE_DIR, 'templates/partials/extrato.html')
+    path_output = BytesIO()
+
+    template_render = render_to_string(path_template, {'valores': valores, 'contas': contas, 'categorias': categorias})
+    HTML(string=template_render).write_pdf(path_output)
+
+    path_output.seek(0)
+    
+
+    return FileResponse(path_output, filename="extrato.pdf")
